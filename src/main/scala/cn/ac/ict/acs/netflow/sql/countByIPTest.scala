670ec84f3920fd80377e97249c83075216ffa863
ayscb <ayscb_022@163.com>
2015-04-08 10:59:46 +0800

    add some sql test

---
+++ src/main/scala/cn/ac/ict/acs/netflow/sql/countByIPTest.scala
@@ -0,0 +1,8 @@
+package cn.ac.ict.acs.netflow.sql
+
+/**
+ * Created by ayscb on 2015/4/7.
+ */
+object countByIPTest {
+
+}
---
+++ src/main/scala/cn/ac/ict/acs/netflow/util/FileUtil.scala
@@ -0,0 +1,8 @@
+package cn.ac.ict.acs.netflow.util
+
+/**
+ * Created by ayscb on 2015/4/7.
+ */
+object FileUtil {
+
+}
