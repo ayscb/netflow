bedfa77abf89d43271c08f166f0aafab912af497
ayscb <ayscb_022@163.com>
2015-04-28 09:40:30 +0800

    delete some useless files

--- src/main/scala/cn/ac/ict/acs/netflow/load2/netFlow/netFlowShema.scala
+++
@@ -1,8 +0,0 @@
-package cn.ac.ict.acs.netflow.load2.netFlow
-
-/**
- * Created by ayscb on 2015/4/14.
- */
-class netFlowShema {
-
-}
--- src/main/scala/cn/ac/ict/acs/netflow/load2/parquetUtil/netflowWriter.scala
+++
@@ -1,8 +0,0 @@
-package cn.ac.ict.acs.netflow.load2.parquetUtil
-
-/**
- * Created by ayscb on 2015/4/18.
- */
-object netflowWriter {
-
-}
