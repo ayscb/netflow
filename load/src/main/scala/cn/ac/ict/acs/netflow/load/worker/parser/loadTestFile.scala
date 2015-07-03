1374cdd51a324b66cfcae60eb5257ce498921a38
ayscb <ayscb_022@163.com>
2015-07-03 10:27:01 +0800

    add the method to prase pcap file into raw netflow package for test

--- load/src/main/scala/cn/ac/ict/acs/netflow/load/worker/parser/loadTestFile.scala
+++ load/src/main/scala/cn/ac/ict/acs/netflow/load/worker/parser/loadTestFile.scala
@@ -18,148 +18,57 @@
  */
 package cn.ac.ict.acs.netflow.load.worker.parser
 
-import java.io.{ DataInputStream, DataOutputStream, FileInputStream, FileOutputStream }
+import java.io._
 import java.nio.ByteBuffer
-import java.util
+import java.nio.channels.FileChannel
 
-/**
- * Created by ayscb on 15-6-13.
- */
 object loadTestFile {
-  // 5C F9 DD 1E 35 76 Start mac
-  private val beginFlag: Array[Byte] = Array[Byte](92, -7, -35, 30, 53, 118)
 
-  private val writeData = new DataOutputStream(new FileOutputStream("/home/ayscb/netflow.data"))
-  private val writeTemp = new DataOutputStream(new FileOutputStream("/home/ayscb/netflow.temp"))
-  private val writeCom = new DataOutputStream(new FileOutputStream("/home/ayscb/netflow.com"))
-  private var count = 0
+  // -----------------------------------------------------
+  // |                 pcap header(24Byte)               |
+  // | record1 - header(16Byte) | record1 - body(16Byte) |
+  // | record2 - header(16Byte) | record1 - body(16Byte) |
+  // | record3 - header(16Byte) | record1 - body(16Byte) |
+  // -----------------------------------------------------
 
-  private var c = 0
-  def findNextRecord(data: DataInputStream,
-                     ResuseArray: Array[Byte],
-                     netflowBuffer: ByteBuffer): Int = {
-    val macLength = 64 // min mac length
-    var find = false
-    while (!find) {
-      while (data.available() > macLength && data.readByte() != beginFlag(0)) {}
-      if (data.available() < macLength) {
-        data.close()
-        return 0 // the data pack is over
-      }
-      ResuseArray(0) = 92
-      data.read(ResuseArray, 1, 5)
-      if (util.Arrays.equals(beginFlag, ResuseArray)) find = true
-    }
-
-    // find the record
-    upPackMac(data, netflowBuffer)
-  }
-
-  private def upPackMac(data: DataInputStream, netflowBuffer: ByteBuffer): Int = {
-    // mac format
-    // ---------------------------------------
-    // src MAC  : 6Byte
-    // dest MAC : 6Byte
-    // type     : 2Byte
-    // body     : 46B ~ 1500B
-    // CRC      : 4Byte
-    // -----------------------------------------
-
-    data.skip(6) // dest MAC
-    val s = data.readShort() // type
-    s match {
-      case 0x0800 => upPackIP(data, netflowBuffer)
-      case _ => -1 // unexpected
-    }
-  }
-
-  // jump the Ip package
-  private def upPackIP(data: DataInputStream, netflowBuffer: ByteBuffer): Int = {
-    val headerLen = (data.readByte() & 0x0f) * 4 // 1Byte ( Ip version + headerLength )
-    data.skipBytes(1) // 1Byte
-    val ipTotalLength = data.readUnsignedShort() // 2Byte
-    val ipBodyLength = ipTotalLength - headerLen
-    data.skip(headerLen - 4)
-    if (data.available() >= ipBodyLength) {
-      upPackUDP(data, netflowBuffer)
-    } else {
-      -1 // unexpected
-    }
-  }
-
-  // udp format
-  // ---------------------------------------
-  // src port : 16bit | dest port : 16bit
-  // dupLength: 16bit | checksum  : 16bit
-  // body ........
-  // -----------------------------------------
-
-  private def upPackUDP(data: DataInputStream, netflowBuffer: ByteBuffer): Int = {
-    c = c + 1
-    data.skipBytes(4)
-    val len = data.readShort() - 8 // except 8 byte's udp header
-    data.skipBytes(2)
-
-    if (len >= 1480) {
-      udpCount += 1
-      println(c + " " + len)
-      data.skipBytes(1480)
-      return -1
-    }
-
-    data.read(netflowBuffer.array(), 0, len)
-    netflowBuffer.limit(len)
-    1
-  }
-  private var udpCount = 0
-  private def upPackNetflow(data: ByteBuffer): Unit = {
-
-    // header 4B * 5 =20
-    // FlowSet 2Byte
-    if (data.getShort(0) != 9) {
-      udpCount += 1
-      println(s"$udpCount---> not netflow package")
-
-      return
-    }
-    val no = data.getShort(20 + data.position())
-    if (no < 256) {
-      count += 1
-      //  println(s"current templeate index $count")
-      writeTemp.writeShort(data.limit())
-      writeTemp.write(data.array(), data.position(), data.remaining())
-    } else {
-      count += 1
-      val length = data.getShort(20 + data.position() + 2) // sizeof(FlowSet) = 2Byte
-      if (length == data.limit() - data.position() - 20) {
-        // only has data flowset
-        writeData.writeShort(data.limit())
-        writeData.write(data.array(), data.position(), data.remaining())
-      } else {
-        // maybe has template dataSet
-        writeCom.writeShort(data.limit())
-        writeCom.write(data.array(), data.position(), data.remaining())
-        //      println(s"com --> $count")
-      }
-    }
-  }
+  val header_length = 24              // 24Byte   fix header length
+  val record_header_len = 16          // 16Byte   a record header
+  val mac_header_len = 6 + 6 + 2      // 14Byte   MAC src + MAC desc + pro
+  val ip_header = 4 * 5               // 20Byte   IP header
+  val udp_header = 2 + 2 + 2 + 2      // 8Byte    UDP header
+  val load_len_pos = record_header_len + mac_header_len + ip_header + 2 + 2
 
   def main(args: Array[String]) {
-    val data = new DataInputStream(new FileInputStream("/home/ayscb/202.97.32.135.pcap"))
-    val reuse: Array[Byte] = new Array[Byte](6)
-    val netflowDataBuffer = ByteBuffer.allocate(1600) // receive the udp package
+    val bf = ByteBuffer.allocate(2)
 
-    while (data.available() > 0) {
-      if (findNextRecord(data, reuse, netflowDataBuffer) == 1) {
-        upPackNetflow(netflowDataBuffer)
-        netflowDataBuffer.clear()
-      }
+    val fci: FileChannel = new RandomAccessFile("/home/ayscb/netflow5.pcap", "r").getChannel
+    val fco = new FileOutputStream("/home/ayscb/netflow5").getChannel
+
+    fci.position(header_length)     // skip file header
+    praser(fci, fco)
+    fci.close()
+    fco.close()
+  }
+
+  /**
+   * decode pcap file as format " length payLoad length payload ......."
+   * @param fin
+   * @param fout
+   */
+  def praser(fin: FileChannel, fout: FileChannel): Unit ={
+    val bf = ByteBuffer.allocate(2)
+    while(true){
+      fin.position(fin.position() + load_len_pos)   // skip to udp length
+      if(fin.read(bf) == -1 ) return                // get payload length
+      fin.position(fin.position() + 2)              // skip 2 byte
+
+      bf.flip()
+      val len = bf.getShort(0) - 8                  // get real length
+      bf.putShort(0,len.asInstanceOf[Short])
+      fout.write(bf)
+      if( fin.transferTo(fin.position(),len,fout) == 0 ) return
+      fin.position(fin.position() + len)
+      bf.clear()
     }
-
-    writeData.flush()
-    writeData.close()
-
-    writeTemp.flush()
-    writeTemp.close()
   }
 }
