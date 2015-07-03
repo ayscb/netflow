fca8b83703d925486520492d5911c02b268a0534
ayscb <ayscb_022@163.com>
2015-07-03 10:36:47 +0800

    modify the type from int to uint32_t

--- collector/conf.h
+++ collector/conf.h
@@ -8,32 +8,31 @@
 #ifndef _CONF_H
 #define _CONF_H
 
+#include "common.h"
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
 struct {
-    struct sockaddr_in * masterIP;
-    int masterNum;
+    struct sockaddr_in * masterIP;      //  current master list
+    uint32_t masterNum;                      //  total master number
 } masterList;
 
 struct {
-    int singleWaitSecond;
-
-    int totalMaxTryNum;
-    int receiverWaitSecond;
-
-    long countIntervalNum;
-    int showCount;		// 1 true 0 fasle
+    uint32_t singleWaitSecond;
+    uint32_t totalMaxTryNum;
+    uint32_t receiverWaitSecond;
 }netflowConf;
 
 struct {
     char testLoadData[100];
     char testLoadTemp[100];
     char testLoadMix[100];
+    char testLoadV5[100];
 }netflowtest;
 
-void configure();
-void setAddress( struct sockaddr_in* add, char* ip, int port );
+BOOLEAN configure(void);
+
 #endif
